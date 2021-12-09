58187788fc1f7046febebe08d4bad67a85d0223c
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-09 20:54:20 -0300

    feat: ensure DbAddAccount return SUCCESS(0) if AddAccountRepository returns true

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,4 +1,5 @@
 import { AddAccount } from '@/domain/usecases'
+import { CreationAccountResult } from '@/domain/models'
 import { Hasher } from '@/data/interfaces/cryptography'
 import { AddAccountRepository } from '@/data/interfaces/db'
 
@@ -11,10 +12,11 @@ export class DbAddAccount implements AddAccount {
   async add (data: AddAccount.Params): Promise<AddAccount.Result> {
     const { password } = data
     const hashedPassword = await this.hasher.hash(password)
-    await this.addAccountRepository.add({
+    const isValid = await this.addAccountRepository.add({
       ...data,
       password: hashedPassword
     })
-    return null
+    const result = isValid ? CreationAccountResult.SUCCESS : CreationAccountResult.ERROR
+    return result
   }
 }
