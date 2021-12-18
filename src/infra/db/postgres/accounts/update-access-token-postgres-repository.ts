82d6ce14dfa50d728774fa8136a8d9c5158d8d25
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-18 20:40:52 -0300

    feat: ensure UpdateAccessTokenPostgresRepository update access token with success

---
+++ src/infra/db/postgres/accounts/update-access-token-postgres-repository.ts
@@ -0,0 +1,17 @@
+import { UpdateAccessTokenRepository } from '@/data/interfaces/db'
+import { PostgresHelper } from '@/infra/db'
+
+export class UpdateAccessTokenPostgresRepository implements UpdateAccessTokenRepository {
+  async updateAccessToken (data: UpdateAccessTokenRepository.Params): Promise<void> {
+    const query = 'UPDATE CLIENTES SET token_acesso = $1 WHERE id_cliente = $2'
+    const params = [data.accessToken, data.id]
+    try {
+      await PostgresHelper.beginTransaction()
+      await PostgresHelper.execute(query, params)
+      await PostgresHelper.commitTransaction()
+    } catch (error) {
+      await PostgresHelper.rollbackTransaction()
+      return null
+    }
+  }
+}
