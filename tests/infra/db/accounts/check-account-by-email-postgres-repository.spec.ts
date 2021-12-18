b054ef5b3dd31decd324eb79a7d94b3fd2800520
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-18 12:08:52 -0300

    test: ensure CheckAccountByEmailPostgresRepository return false if account not exists

--- tests/infra/db/accounts/check-account-by-email-postgres-repository.spec.ts
+++ tests/infra/db/accounts/check-account-by-email-postgres-repository.spec.ts
@@ -1,5 +1,6 @@
 import { CheckAccountByEmailPostgresRepository, PostgresHelper } from '@/infra/db'
 import { createDbTest, sqlClearDb, sqlCreateDb, mockAccount } from '@/tests/infra/mocks'
+import faker from 'faker'
 
 const makeSut = (): CheckAccountByEmailPostgresRepository => {
   return new CheckAccountByEmailPostgresRepository()
@@ -23,5 +24,11 @@ describe('Check Account By Email Postgres Repository', () => {
     const params = await mockAccount()
     const response = await sut.check(params.email)
     expect(response).toBe(true)
+  })
+  test('should CheckAccountByEmailPostgresRepository return false if account not exists', async () => {
+    const sut = makeSut()
+    const emailFake = faker.internet.email()
+    const response = await sut.check(emailFake)
+    expect(response).toBe(false)
   })
 })
