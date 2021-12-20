15800061a60891750d5bb63781fa9fda3971e555
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-20 12:27:28 -0300

    test: ensure BcryptAdapter throw error if bcrypt throws

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,4 +1,5 @@
 import { BcryptAdapter } from '@/infra/cryptography'
+import { throwError } from '@/tests/domain/mocks'
 import bcrypt from 'bcrypt'
 import faker from 'faker'
 
@@ -9,6 +10,7 @@ jest.mock('bcrypt', () => ({
 }))
 
 const salt = 12
+const fakePassword = faker.random.word()
 const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
 }
@@ -17,8 +19,13 @@ describe('Bcrypt Adapter', () => {
   test('should BcryptAdapter call bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    const fakePassword = faker.random.word()
     await sut.hash(fakePassword)
     expect(hashSpy).toHaveBeenCalledWith(fakePassword, salt)
+  })
+  test('should BcryptAdapter throw error if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+    const promise = sut.hash(fakePassword)
+    await expect(promise).rejects.toThrow()
   })
 })
