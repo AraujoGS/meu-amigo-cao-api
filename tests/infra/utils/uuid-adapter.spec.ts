0a184fb04dafd9c07d1c1f80192ab64b3a8387e6
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-20 12:38:53 -0300

    ensure UuidAdapter throw error if uuid throws

--- tests/infra/utils/uuid-adapter.spec.ts
+++ tests/infra/utils/uuid-adapter.spec.ts
@@ -1,4 +1,5 @@
 import { UuidAdapter } from '@/infra/utils'
+import { throwError } from '@/tests/domain/mocks'
 import uuid from 'uuid'
 import faker from 'faker'
 
@@ -18,6 +19,11 @@ describe('UUID Adapter', () => {
     const uuidSpy = jest.spyOn(uuid, 'v4')
     sut.generate()
     expect(uuidSpy).toHaveBeenCalled()
+  })
+  test('should UuidAdapter throw error if uuid throws', () => {
+    const sut = makeSut()
+    jest.spyOn(uuid, 'v4').mockImplementationOnce(throwError)
+    expect(() => sut.generate()).toThrow()
   })
   test('should UuidAdapter generate unique identifier valid', () => {
     const sut = makeSut()
