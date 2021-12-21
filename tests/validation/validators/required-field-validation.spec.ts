e0e708ce2fea2dc3c3e6822572411eed28e41d96
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-21 18:54:04 -0300

    test: ensure RequiredFieldValidation return error if missing parameter

---
+++ tests/validation/validators/required-field-validation.spec.ts
@@ -0,0 +1,13 @@
+import { MissingParamError } from '@/presentation/errors'
+import { RequiredFieldValidation } from '@/validation/validators'
+import faker from 'faker'
+
+const field = faker.random.word()
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(field)
+describe('Required Field Validation', () => {
+  test('should RequiredFieldValidation return error if missing parameter', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any: 'any_value' })
+    expect(error).toEqual(new MissingParamError(field))
+  })
+})
