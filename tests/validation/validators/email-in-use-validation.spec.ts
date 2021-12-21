6f107e2e40110956a77b2f79427b38d8e25fe59d
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-21 20:29:06 -0300

    test: ensure EmailInUseValidation return error if email in use

---
+++ tests/validation/validators/email-in-use-validation.spec.ts
@@ -0,0 +1,13 @@
+import { EmailInUseValidation } from '@/validation/validators'
+import { CreationAccountResult } from '@/domain/models'
+import { EmailInUseError } from '@/presentation/errors'
+
+const makeSut = (): EmailInUseValidation => new EmailInUseValidation('resultAddAccount')
+
+describe('Email In Use Validation', () => {
+  test('should EmailInUseValidation return error if email in use', () => {
+    const sut = makeSut()
+    const error = sut.validate({ resultAddAccount: CreationAccountResult.ERROR_EMAIL })
+    expect(error).toEqual(new EmailInUseError())
+  })
+})
