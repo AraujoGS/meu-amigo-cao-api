613df7f95f87a2f3a2040f2d2d827d7a19baac61
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-21 17:45:10 -0300

    feat: ensure EmailValidation return error if EmailValidator return false

--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -1,10 +1,13 @@
+import { InvalidParamError } from '@/presentation/errors'
 import { Validation } from '@/presentation/interfaces'
 import { EmailValidator } from '@/validation/interfaces'
 
 export class EmailValidation implements Validation {
   constructor (private readonly emailValidator: EmailValidator) {}
   validate (input: any): Error {
-    this.emailValidator.isValid(input.email)
-    return null
+    const isValid = this.emailValidator.isValid(input.email)
+    if (!isValid) {
+      return new InvalidParamError('email')
+    }
   }
 }
