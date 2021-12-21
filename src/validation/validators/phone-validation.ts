3f83266dc4e8d279811413ea5ffb53ea18f488d7
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-21 18:22:45 -0300

    feat: ensure PhoneValidation return error if PhoneValidator return false

--- src/validation/validators/phone-validation.ts
+++ src/validation/validators/phone-validation.ts
@@ -1,10 +1,13 @@
+import { InvalidParamError } from '@/presentation/errors'
 import { Validation } from '@/presentation/interfaces'
 import { PhoneValidator } from '@/validation/interfaces'
 
 export class PhoneValidation implements Validation {
   constructor (private readonly phoneValidator: PhoneValidator) {}
   validate (input: any): Error {
-    this.phoneValidator.isValid(input.phone)
-    return null
+    const isValid = this.phoneValidator.isValid(input.phone)
+    if (!isValid) {
+      return new InvalidParamError('phone')
+    }
   }
 }
