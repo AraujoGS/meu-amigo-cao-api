79b3f6c0229280d2702ed91e520de6ead763237c
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-24 15:20:34 -0300

    test: ensure signup route return 412 if fail

--- tests/presentation/routes/account-routes.test.ts
+++ tests/presentation/routes/account-routes.test.ts
@@ -3,8 +3,9 @@ import { setupApp } from '@/main/config/app'
 import { PostgresHelper } from '@/infra/db'
 import { createDbTest, sqlClearDb, sqlCreateDb } from '@/tests/infra/mocks'
 import { Express } from 'express'
+import { hash } from 'bcrypt'
+import { v4 as uuid } from 'uuid'
 import request from 'supertest'
-import faker from 'faker'
 let app: Express = null
 
 describe('Account Routes', () => {
@@ -21,19 +22,44 @@ describe('Account Routes', () => {
   })
   describe('POST /signup', () => {
     test('should signup route return 201 if success', async () => {
-      const fakePwd = faker.random.alphaNumeric(12)
+      const fakePwd = '123'
       const payload = {
-        name: faker.name.findName(),
-        email: faker.internet.email(),
+        name: 'Guilherme de Araujo',
+        email: 'guilhermearaujo421@gmail.com',
         password: fakePwd,
         passwordConfirmation: fakePwd,
-        phone: faker.phone.phoneNumber('##9########'),
-        birthDate: faker.date.past().toLocaleDateString('pt-br').split('/').reverse().join('-')
+        phone: '11954976863',
+        birthDate: '1997-05-30'
       }
       await request(app)
         .post('/api/signup')
         .send(payload)
         .expect(201)
+    })
+    test('should signup route return 412 if fail', async () => {
+      const password = await hash('123', 12)
+      const phone = '11954976863'
+      const id = uuid()
+      const query = `
+      INSERT INTO CLIENTES(id_cliente, nome_cliente, senha_cliente, email_cliente, telefone_cliente, data_nascimento_cliente)
+      VALUES ($1,$2,$3,$4,$5,$6)
+      `
+      const params = [id, 'Guilherme de Araujo', password, 'guilhermearaujo422@gmail.com', phone, '1997-05-30']
+      await PostgresHelper.execute(query, params)
+
+      const fakePwd = '123'
+      const payload = {
+        name: 'Guilherme de Araujo',
+        email: 'guilhermearaujo421@gmail.com',
+        password: fakePwd,
+        passwordConfirmation: fakePwd,
+        phone,
+        birthDate: '1997-05-30'
+      }
+      await request(app)
+        .post('/api/signup')
+        .send(payload)
+        .expect(412)
     })
   })
 })
