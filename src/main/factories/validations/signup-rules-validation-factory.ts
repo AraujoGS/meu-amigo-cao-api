23ad91004d3185c835cfbb5e06e67db973249237
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-24 15:50:39 -0300

    fix: add creationAccountValidation if signupRulesValidation

--- src/main/factories/validations/signup-rules-validation-factory.ts
+++ src/main/factories/validations/signup-rules-validation-factory.ts
@@ -1,9 +1,10 @@
 import { Validation } from '@/presentation/interfaces'
-import { EmailInUseValidation, PhoneInUseValidation, ValidationComposite } from '@/validation/validators'
+import { CreationAccountValidation, EmailInUseValidation, PhoneInUseValidation, ValidationComposite } from '@/validation/validators'
 
 export const makeSignUpRulesValidation = (): Validation => {
   const validations: Validation[] = []
   validations.push(new EmailInUseValidation('resultAddAccount'))
   validations.push(new PhoneInUseValidation('resultAddAccount'))
+  validations.push(new CreationAccountValidation('resultAddAccount'))
   return new ValidationComposite(validations)
 }
