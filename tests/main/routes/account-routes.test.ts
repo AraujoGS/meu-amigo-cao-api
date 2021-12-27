f9a20bba4605c2f6f24edd6058ad7540c357a284
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-27 20:47:38 -0300

    test: ensure login route return 200 if success

--- tests/main/routes/account-routes.test.ts
+++ tests/main/routes/account-routes.test.ts
@@ -8,6 +8,18 @@ import { v4 as uuid } from 'uuid'
 import request from 'supertest'
 let app: Express = null
 
+const mockAddAccount = async (): Promise<void> => {
+  const password = await hash('123', 12)
+  const phone = '11954976863'
+  const id = uuid()
+  const query = `
+  INSERT INTO CLIENTES(id_cliente, nome_cliente, senha_cliente, email_cliente, telefone_cliente, data_nascimento_cliente)
+  VALUES ($1,$2,$3,$4,$5,$6)
+  `
+  const params = [id, 'Guilherme de Araujo', password, 'guilhermearaujo421@gmail.com', phone, '1997-05-30']
+  await PostgresHelper.execute(query, params)
+}
+
 describe('Account Routes', () => {
   beforeAll(async () => {
     app = await setupApp()
@@ -51,29 +63,32 @@ describe('Account Routes', () => {
         .expect(400)
     })
     test('should signup route return 412 if fail', async () => {
-      const password = await hash('123', 12)
-      const phone = '11954976863'
-      const id = uuid()
-      const query = `
-      INSERT INTO CLIENTES(id_cliente, nome_cliente, senha_cliente, email_cliente, telefone_cliente, data_nascimento_cliente)
-      VALUES ($1,$2,$3,$4,$5,$6)
-      `
-      const params = [id, 'Guilherme de Araujo', password, 'guilhermearaujo422@gmail.com', phone, '1997-05-30']
-      await PostgresHelper.execute(query, params)
-
+      await mockAddAccount()
       const fakePwd = '123'
       const payload = {
         name: 'Guilherme de Araujo',
         email: 'guilhermearaujo421@gmail.com',
         password: fakePwd,
         passwordConfirmation: fakePwd,
-        phone,
+        phone: '11954976863',
         birthDate: '1997-05-30'
       }
       await request(app)
         .post('/api/signup')
         .send(payload)
         .expect(412)
+    })
+  })
+  describe('POST /login', () => {
+    test('should login route return 200 if success', async () => {
+      await mockAddAccount()
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'guilhermearaujo421@gmail.com',
+          password: '123'
+        })
+        .expect(200)
     })
   })
 })
