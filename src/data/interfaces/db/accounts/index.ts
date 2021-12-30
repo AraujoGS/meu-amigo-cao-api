ad689f74398846abfdd235ab23ebe8d00a2b95e9
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-30 17:55:47 -0300

    refactor: move interfaces db account to a folder

---
+++ src/data/interfaces/db/accounts/index.ts
@@ -0,0 +1,6 @@
+export * from './add-account-repository'
+export * from './check-account-by-email-repository'
+export * from './check-account-by-phone-repository'
+export * from './load-account-by-email-repository'
+export * from './update-access-token-repository'
+export * from './load-account-by-email-and-phone-repository'
