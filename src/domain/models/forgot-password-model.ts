80a182df5afd0009375bba11fe09d89721c06e2a
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-30 18:19:13 -0300

    feat: ensure LoadAccountByEmailAndPhonePostgresRepository return data if account exists

--- src/domain/models/forgot-password-model.ts
+++ src/domain/models/forgot-password-model.ts
@@ -1,4 +1,4 @@
 export type ForgotPasswordModel = {
   email: string
-  telefone: string
+  phone: string
 }
---
+++ src/infra/db/postgres/accounts/load-account-by-email-and-phone-postgres-repository.ts
@@ -0,0 +1,12 @@
+import { LoadAccountByEmailAndPhoneRepository } from '@/data/interfaces/db'
+import { PostgresHelper } from '@/infra/db'
+
+export class LoadAccountByEmailAndPhonePostgresRepository implements LoadAccountByEmailAndPhoneRepository {
+  async loadByEmailAndPhone (data: LoadAccountByEmailAndPhoneRepository.Params): Promise<LoadAccountByEmailAndPhoneRepository.Result> {
+    const query = 'SELECT nome_cliente as name, email_cliente as email FROM CLIENTES WHERE email_cliente = $1 AND telefone_cliente = $2'
+    const { email, phone } = data
+    const params = [email, phone]
+    const account = await PostgresHelper.execute(query, params)
+    return account.rows[0]
+  }
+}
