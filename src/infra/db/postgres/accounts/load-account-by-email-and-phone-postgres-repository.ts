6c02dfcd151009cb34ea2d397115244db5d74a5f
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-30 18:23:09 -0300

    feat: ensure LoadAccountByEmailAndPhonePostgresRepository return null if account not exists

--- src/infra/db/postgres/accounts/load-account-by-email-and-phone-postgres-repository.ts
+++ src/infra/db/postgres/accounts/load-account-by-email-and-phone-postgres-repository.ts
@@ -7,6 +7,6 @@ export class LoadAccountByEmailAndPhonePostgresRepository implements LoadAccount
     const { email, phone } = data
     const params = [email, phone]
     const account = await PostgresHelper.execute(query, params)
-    return account.rows[0]
+    return account?.rows.length ? account.rows[0] : null
   }
 }
