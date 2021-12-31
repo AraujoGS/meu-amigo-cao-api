b5ff2734d78f63facdcdea538b2346d80d720b58
Guilherme AS <guilhermearaujo421@gmail.com>
2021-12-31 12:38:50 -0300

    refactor: add bcrypt adapter to composition forgot password usecase

--- src/main/factories/usecases/db-forgot-password-factory.ts
+++ src/main/factories/usecases/db-forgot-password-factory.ts
@@ -2,13 +2,22 @@ import { DbForgotPassword } from '@/data/usecases'
 import { ForgotPassword } from '@/domain/usecases'
 import { LoadAccountByEmailAndPhonePostgresRepository, UpdatePasswordPostgresRepository } from '@/infra/db'
 import { PasswordGeneratorAdapter } from '@/infra/utils'
+import { BcryptAdapter } from '@/infra/cryptography'
 import { SendEmailRecoverPasswordNodemailer } from '@/infra/comunication'
 
 export const makeDbForgotPassword = (): ForgotPassword => {
   const loadAccountByEmailAndPhonePostgresRepository = new LoadAccountByEmailAndPhonePostgresRepository()
   const lengthPassword = 12
   const passwordGeneratorAdapter = new PasswordGeneratorAdapter(lengthPassword)
+  const salt = parseInt(process.env.SALT)
+  const bcryptAdapter = new BcryptAdapter(salt)
   const updatePasswordPostgresRepository = new UpdatePasswordPostgresRepository()
   const sendEmailRecoverPasswordNodemailer = new SendEmailRecoverPasswordNodemailer()
-  return new DbForgotPassword(loadAccountByEmailAndPhonePostgresRepository, passwordGeneratorAdapter, updatePasswordPostgresRepository, sendEmailRecoverPasswordNodemailer)
+  return new DbForgotPassword(
+    loadAccountByEmailAndPhonePostgresRepository,
+    passwordGeneratorAdapter,
+    bcryptAdapter,
+    updatePasswordPostgresRepository,
+    sendEmailRecoverPasswordNodemailer
+  )
 }
