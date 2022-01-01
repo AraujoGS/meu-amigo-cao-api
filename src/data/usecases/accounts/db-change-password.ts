ce8d8feb24d9e69fcb9387a1387768cd82eb0ce5
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-01 20:01:32 -0300

    feat: ensure DbChangePassword call Hasher with correct password

--- src/data/usecases/accounts/db-change-password.ts
+++ src/data/usecases/accounts/db-change-password.ts
@@ -1,20 +1,22 @@
 import { LoadAccountByIdRepository } from '@/data/interfaces/db'
-import { HashComparer } from '@/data/interfaces/cryptography'
+import { HashComparer, Hasher } from '@/data/interfaces/cryptography'
 import { ChangePasswordResult } from '@/domain/models'
 import { ChangePassword } from '@/domain/usecases'
 
 export class DbChangePassword implements ChangePassword {
   constructor (
     private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly hasher: Hasher
   ) {}
 
   async change (data: ChangePassword.Params): Promise<ChangePassword.Result> {
-    const { id, oldPassword } = data
+    const { id, oldPassword, newPassword } = data
     const account = await this.loadAccountByIdRepository.loadById(id)
     if (account) {
       const isValid = await this.hashComparer.compare({ value: oldPassword, hash: account.password })
       if (!isValid) return ChangePasswordResult.ERROR_INVALID_PASSWORD
+      await this.hasher.hash(newPassword)
     }
     return ChangePasswordResult.ERROR_ACCOUNT_NOT_EXISTS
   }
