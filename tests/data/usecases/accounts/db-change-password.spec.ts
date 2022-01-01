ed48802e3c658a09570b9664a21b0dd4a726fb8c
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-01 19:42:42 -0300

    test: ensure DbChangePassword return ERROR_ACCOUNT_NOT_EXISTS(3) if LoadAccountByIdRepository return null

--- tests/data/usecases/accounts/db-change-password.spec.ts
+++ tests/data/usecases/accounts/db-change-password.spec.ts
@@ -1,4 +1,5 @@
 import { DbChangePassword } from '@/data/usecases'
+import { ChangePasswordResult } from '@/domain/models'
 import { mockChangePasswordParams } from '@/tests/domain/mocks'
 import { LoadAccountByIdRepositorySpy } from '@/tests/data/mocks'
 
@@ -21,5 +22,11 @@ describe('DbChangePassword Usecase', () => {
     const params = mockChangePasswordParams()
     await sut.change(params)
     expect(loadAccountByIdRepositorySpy.id).toEqual(params.id)
+  })
+  it('should DbChangePassword return ERROR_ACCOUNT_NOT_EXISTS(3) if LoadAccountByIdRepository return null', async () => {
+    const { sut, loadAccountByIdRepositorySpy } = makeSut()
+    loadAccountByIdRepositorySpy.result = null
+    const result = await sut.change(mockChangePasswordParams())
+    expect(result).toEqual(ChangePasswordResult.ERROR_ACCOUNT_NOT_EXISTS)
   })
 })
