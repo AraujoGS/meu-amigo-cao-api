c76eea93a98aa9de128850f49cc680f423656103
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-01 23:42:56 -0300

    test: ensure PasswordEqualsValidation return error if password equals

---
+++ tests/validation/validators/password-equals-validation.spec.ts
@@ -0,0 +1,11 @@
+import { PasswordEqualsError } from '@/presentation/errors'
+import { PasswordEqualsValidation } from '@/validation/validators'
+
+const makeSut = (): PasswordEqualsValidation => new PasswordEqualsValidation('field', 'field2')
+describe('Password Equals Validation', () => {
+  it('should PasswordEqualsValidation return error if password equals', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', field2: 'any_value' })
+    expect(error).toEqual(new PasswordEqualsError())
+  })
+})
