9defa1d445a0603ef96e9627e52dc821b4f60fba
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-01 23:39:37 -0300

    test: ensure AccountNotExistsValidation return error if account not exists

---
+++ tests/validation/validators/account-not-exists-validation.spec.ts
@@ -0,0 +1,13 @@
+import { AccountNotExistsValidation } from '@/validation/validators'
+import { ChangePasswordResult } from '@/domain/models'
+import { AccountNotExistsError } from '@/presentation/errors'
+
+const makeSut = (): AccountNotExistsValidation => new AccountNotExistsValidation('resultChangePassword')
+
+describe('Account Not Exists Validation', () => {
+  it('should AccountNotExistsValidation return error if account not exists', () => {
+    const sut = makeSut()
+    const error = sut.validate({ resultChangePassword: ChangePasswordResult.ERROR_ACCOUNT_NOT_EXISTS })
+    expect(error).toEqual(new AccountNotExistsError())
+  })
+})
