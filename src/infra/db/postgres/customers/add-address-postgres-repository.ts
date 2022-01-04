87153e9dcbf59e31c736dbaf51a24019c26ed151
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-03 21:14:39 -0300

    feat: ensure AddAddressPostgresRepository add address with success

--- src/infra/db/postgres/customers/add-address-postgres-repository.ts
+++ src/infra/db/postgres/customers/add-address-postgres-repository.ts
@@ -1,5 +1,6 @@
-import { IdentifierGenerator } from '@/data/interfaces/utils'
 import { AddAddressRepository } from '@/data/interfaces/db'
+import { IdentifierGenerator } from '@/data/interfaces/utils'
+import { PostgresHelper } from '@/infra/db'
 
 export class AddAddressPostgresRepository implements AddAddressRepository {
   constructor (
@@ -7,6 +8,30 @@ export class AddAddressPostgresRepository implements AddAddressRepository {
   ) {}
 
   async add (data: AddAddressRepository.Params): Promise<void> {
-    this.identifierGenerator.generate()
+    const query = `
+    INSERT INTO ENDERECOS(
+      id_endereco,
+      id_cliente, 
+      cep, 
+      logradouro, 
+      cidade, 
+      bairro,
+      numero,
+      uf,
+      complemento  
+    )
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
+    `
+    const { accountId, zipcode, address, city, district, number, state, complement } = data
+    const id = this.identifierGenerator.generate()
+    const params = [id, accountId, zipcode, address, city, district, number, state, complement]
+    try {
+      await PostgresHelper.beginTransaction()
+      await PostgresHelper.execute(query, params)
+      await PostgresHelper.commitTransaction()
+    } catch (error) {
+      await PostgresHelper.rollbackTransaction()
+      throw error
+    }
   }
 }
