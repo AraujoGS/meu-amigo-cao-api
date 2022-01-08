55ec0218b66de39f40a60f02565637b2fbc8802b
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-08 11:30:50 -0300

    test: ensure DbAddPet call CheckDogBreedByIdRepository with correct dog breed id

--- tests/data/mocks/pets.ts
+++ tests/data/mocks/pets.ts
@@ -1,4 +1,4 @@
-import { AddPetRepository } from '@/data/interfaces/db'
+import { AddPetRepository, CheckDogBreedByIdRepository } from '@/data/interfaces/db'
 import faker from 'faker'
 faker.locale = 'pt_BR'
 
@@ -7,5 +7,14 @@ export class AddPetRepositorySpy implements AddPetRepository {
   async add (data: AddPetRepository.Params): Promise<void> {
     this.data = data
     return await Promise.resolve()
+  }
+}
+
+export class CheckDogBreedByIdRepositorySpy implements CheckDogBreedByIdRepository {
+  id: CheckDogBreedByIdRepository.Params
+  result = true
+  async check (id: CheckDogBreedByIdRepository.Params): Promise<CheckDogBreedByIdRepository.Result> {
+    this.id = id
+    return this.result
   }
 }
--- tests/data/usecases/pets/db-add-pet.spec.ts
+++ tests/data/usecases/pets/db-add-pet.spec.ts
@@ -1,23 +1,32 @@
 import { DbAddPet } from '@/data/usecases'
 import { ActionResult } from '@/domain/models'
-import { AddPetRepositorySpy } from '@/tests/data/mocks'
+import { AddPetRepositorySpy, CheckDogBreedByIdRepositorySpy } from '@/tests/data/mocks'
 import { mockAddPetParams, throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbAddPet
   addPetRepositorySpy: AddPetRepositorySpy
+  checkDobBreedByIdRepositorySpy: CheckDogBreedByIdRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const addPetRepositorySpy = new AddPetRepositorySpy()
-  const sut = new DbAddPet(addPetRepositorySpy)
+  const checkDobBreedByIdRepositorySpy = new CheckDogBreedByIdRepositorySpy()
+  const sut = new DbAddPet(checkDobBreedByIdRepositorySpy, addPetRepositorySpy)
   return {
     sut,
-    addPetRepositorySpy
+    addPetRepositorySpy,
+    checkDobBreedByIdRepositorySpy
   }
 }
 
 describe('DbAddPet Usecase', () => {
+  it('should DbAddPet call CheckDogBreedByIdRepository with correct dog breed id', async () => {
+    const { sut, checkDobBreedByIdRepositorySpy } = makeSut()
+    const params = mockAddPetParams()
+    await sut.add(params)
+    expect(checkDobBreedByIdRepositorySpy.id).toEqual(params.breed)
+  })
   it('should DbAddPet call AddPetRepositorySpy with correct values', async () => {
     const { sut, addPetRepositorySpy } = makeSut()
     const params = mockAddPetParams()
