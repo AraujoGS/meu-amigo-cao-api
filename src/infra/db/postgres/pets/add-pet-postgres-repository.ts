e26334da2a3bddfb18de5b13ba95dfbd2cdc7bfd
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-08 20:36:29 -0300

    feat: ensure AddPetPostgresRepository add pet with success

--- src/infra/db/postgres/pets/add-pet-postgres-repository.ts
+++ src/infra/db/postgres/pets/add-pet-postgres-repository.ts
@@ -1,5 +1,6 @@
 import { AddPetRepository } from '@/data/interfaces/db'
 import { IdentifierGenerator } from '@/data/interfaces/utils'
+import { PostgresHelper } from '@/infra/db'
 
 export class AddPetPostgresRepository implements AddPetRepository {
   constructor (
@@ -7,6 +8,28 @@ export class AddPetPostgresRepository implements AddPetRepository {
   ) {}
 
   async add (data: AddPetRepository.Params): Promise<void> {
-    this.identifierGenerator.generate()
+    const query = `
+    INSERT INTO PETS(
+      id_pet,
+      nome_pet, 
+      cor_pet, 
+      consideracoes, 
+      id_cliente,
+      id_raca,
+      id_porte
+    )
+    VALUES ($1,$2,$3,$4,$5,$6,$7)
+    `
+    const { name, color, considerations, accountId, breed, type } = data
+    const id = this.identifierGenerator.generate()
+    const params = [id, name, color, considerations, accountId, breed, type]
+    try {
+      await PostgresHelper.beginTransaction()
+      await PostgresHelper.execute(query, params)
+      await PostgresHelper.commitTransaction()
+    } catch (error) {
+      await PostgresHelper.rollbackTransaction()
+      throw error
+    }
   }
 }
