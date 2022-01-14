a9a1dfafa46aa31bf51b4c7ada740197d5b4109b
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-14 18:14:25 -0300

    chore: release 1.7.0

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,7 @@ export default {
   info: {
     title: 'Meu Amigo Cão API',
     description: 'API para o petshop meu amigo cão, através dela são feitas as modificações nos dados dos clientes',
-    version: '1.6.0'
+    version: '1.7.0'
   },
   servers: [{
     url: '/api'
