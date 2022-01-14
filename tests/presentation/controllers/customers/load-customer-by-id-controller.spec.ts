8bb42e92c999e952c3ae90ae6be32e17f94f33e4
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-14 17:07:27 -0300

    test: ensure LoadCustomerByIdController return 200 if LoadCustomerById returns

--- tests/presentation/controllers/customers/load-customer-by-id-controller.spec.ts
+++ tests/presentation/controllers/customers/load-customer-by-id-controller.spec.ts
@@ -1,4 +1,5 @@
 import { LoadCustomerByIdController } from '@/presentation/controllers'
+import { ok } from '@/presentation/helpers'
 import { mockLoadCustomerById } from '@/tests/domain/mocks'
 import { LoadCustomerByIdSpy } from '@/tests/presentation/mocks'
 
@@ -23,5 +24,10 @@ describe('LoadCustomerById Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadCustomerByIdSpy.id).toBe(request.accountId)
+  })
+  it('should LoadCustomerByIdController return 200 if LoadCustomerById returns', async () => {
+    const { sut, loadCustomerByIdSpy } = makeSut()
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(ok(loadCustomerByIdSpy.result))
   })
 })
