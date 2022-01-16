503899b7de5b2f5995d4c18a34be8eadff63dc77
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-16 13:58:46 -0300

    feat: ensure DbChangeAddress return false if LoadAddressByIdAndCustomerIdRepository return false

--- src/data/usecases/customers/db-change-address.ts
+++ src/data/usecases/customers/db-change-address.ts
@@ -9,6 +9,6 @@ export class DbChangeAddress implements ChangeAddress {
   async change (data: ChangeAddress.Params): Promise<ChangeAddress.Result> {
     const { id, accountId } = data
     await this.checkAddressByIdAndCustomerIdRepository.check({ id, accountId })
-    return null
+    return false
   }
 }
