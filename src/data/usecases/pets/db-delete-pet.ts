22139a449b7d7d7f24c2d68b0f191f87f0ad37b2
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-21 11:15:12 -0300

    feat: ensure DbDeletePet call DeletePetRepository with correct values

--- src/data/usecases/pets/db-delete-pet.ts
+++ src/data/usecases/pets/db-delete-pet.ts
@@ -1,13 +1,17 @@
-import { CheckPetByIdAndCustomerIdRepository } from '@/data/interfaces/db'
+import { CheckPetByIdAndCustomerIdRepository, DeletePetRepository } from '@/data/interfaces/db'
 import { DeletePet } from '@/domain/usecases'
 
 export class DbDeletePet implements DeletePet {
   constructor (
-    private readonly checkPetByIdAndCustomerIdRepository: CheckPetByIdAndCustomerIdRepository
+    private readonly checkPetByIdAndCustomerIdRepository: CheckPetByIdAndCustomerIdRepository,
+    private readonly deletePetRepository: DeletePetRepository
   ) {}
 
   async delete (data: DeletePet.Params): Promise<DeletePet.Result> {
-    await this.checkPetByIdAndCustomerIdRepository.check(data)
+    const isValid = await this.checkPetByIdAndCustomerIdRepository.check(data)
+    if (isValid) {
+      await this.deletePetRepository.delete(data)
+    }
     return false
   }
 }
