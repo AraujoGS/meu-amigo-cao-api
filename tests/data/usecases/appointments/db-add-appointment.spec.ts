28203a44c491fcd94d7f00bd931a137a86727243
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-21 21:24:54 -0300

    test: ensure DbAddAppointment return ERROR_PET_NOT_EXISTS (8) if CheckPetByIdAndCustomerIdRepository return false

--- tests/data/usecases/appointments/db-add-appointment.spec.ts
+++ tests/data/usecases/appointments/db-add-appointment.spec.ts
@@ -1,4 +1,5 @@
 import { DbAddAppointment } from '@/data/usecases'
+import { ActionResult } from '@/domain/models'
 import { CheckPetByIdAndCustomerIdRepositorySpy } from '@/tests/data/mocks'
 import { mockAddAppointments, throwError } from '@/tests/domain/mocks'
 
@@ -17,7 +18,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbAddAppointment Usecase', () => {
-  it('should DbAddAppointment call LoadPetByIdAndCustomerIdRepository with correct values', async () => {
+  it('should DbAddAppointment call CheckPetByIdAndCustomerIdRepository with correct values', async () => {
     const { sut, checkPetByIdAndCustomerIdRepositorySpy } = makeSut()
     const params = mockAddAppointments()
     await sut.add(params)
@@ -26,10 +27,16 @@ describe('DbAddAppointment Usecase', () => {
       accountId: params.accountId
     })
   })
-  it('should DbAddAppointment throw error if LoadPetByIdAndCustomerIdRepository throws', async () => {
+  it('should DbAddAppointment throw error if CheckPetByIdAndCustomerIdRepository throws', async () => {
     const { sut, checkPetByIdAndCustomerIdRepositorySpy } = makeSut()
     jest.spyOn(checkPetByIdAndCustomerIdRepositorySpy, 'check').mockImplementationOnce(throwError)
     const promise = sut.add(mockAddAppointments())
     await expect(promise).rejects.toThrow()
+  })
+  it('should DbAddAppointment return ERROR_PET_NOT_EXISTS (8) if CheckPetByIdAndCustomerIdRepository return false', async () => {
+    const { sut, checkPetByIdAndCustomerIdRepositorySpy } = makeSut()
+    checkPetByIdAndCustomerIdRepositorySpy.result = false
+    const result = await sut.add(mockAddAppointments())
+    expect(result).toBe(ActionResult.ERROR_PET_NOT_EXISTS)
   })
 })
