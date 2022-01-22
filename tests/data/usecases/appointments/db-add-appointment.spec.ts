ff66f3458ab716b52bbd74aba7c2e6f870fcedb1
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-21 21:16:35 -0300

    test: ensure DbAddAppointment throw error if LoadPetByIdAndCustomerIdRepository throws

--- tests/data/usecases/appointments/db-add-appointment.spec.ts
+++ tests/data/usecases/appointments/db-add-appointment.spec.ts
@@ -1,6 +1,6 @@
 import { DbAddAppointment } from '@/data/usecases'
 import { CheckPetByIdAndCustomerIdRepositorySpy } from '@/tests/data/mocks'
-import { mockAddAppointments } from '@/tests/domain/mocks'
+import { mockAddAppointments, throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbAddAppointment
@@ -25,5 +25,11 @@ describe('DbAddAppointment Usecase', () => {
       id: params.petId,
       accountId: params.accountId
     })
+  })
+  it('should DbAddAppointment throw error if LoadPetByIdAndCustomerIdRepository throws', async () => {
+    const { sut, checkPetByIdAndCustomerIdRepositorySpy } = makeSut()
+    jest.spyOn(checkPetByIdAndCustomerIdRepositorySpy, 'check').mockImplementationOnce(throwError)
+    const promise = sut.add(mockAddAppointments())
+    await expect(promise).rejects.toThrow()
   })
 })
