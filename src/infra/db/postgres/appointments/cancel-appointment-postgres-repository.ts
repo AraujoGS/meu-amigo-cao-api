7aa00b8a6a264eda502b9ce4833abae3a63b900e
Guilherme AS <guilhermearaujo421@gmail.com>
2022-01-28 20:35:56 -0300

    feat: add CancelAppointmentPostgresRepository

---
+++ src/infra/db/postgres/appointments/cancel-appointment-postgres-repository.ts
@@ -0,0 +1,17 @@
+import { CancelAppointmentRepository } from '@/data/interfaces/db'
+import { PostgresHelper } from '@/infra/db'
+
+export class CancelAppointmentRepositoryPostgresRepository implements CancelAppointmentRepository {
+  async cancel (id: CancelAppointmentRepository.Params): Promise<void> {
+    const query = 'UPDATE AGENDAMENTOS SET cancelamento = true WHERE id_agendamento = $1'
+    const params = [id]
+    try {
+      await PostgresHelper.beginTransaction()
+      await PostgresHelper.execute(query, params)
+      await PostgresHelper.commitTransaction()
+    } catch (error) {
+      await PostgresHelper.rollbackTransaction()
+      throw error
+    }
+  }
+}
